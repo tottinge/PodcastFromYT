a125677ae88345bce690692467475bc6f78ebc91
Tim Ottinger <tottinge@industriallogic.com>
2020-04-29 11:39:14 -0500

    add limit parameter to download.sh

--- download.sh
+++ download.sh
@@ -1,5 +1,7 @@
+echo "Args are 0:$0, 1:$1, 2:$2"
+limit=${1:-2}
 youtube-dl --extract-audio \
     --audio-format mp3 \
     --write-info-json \
-    --max-downloads 4  \
+    --max-downloads ${limit}  \
     https://www.youtube.com/channel/UCMwCSEyUk59V8IQADpdn5PA
